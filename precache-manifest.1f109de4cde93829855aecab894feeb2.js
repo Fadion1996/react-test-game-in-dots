e392ddb7250eda0891a12a0ca59884aa12b507b3
Vasiliy Yurkovets <yurkovets@Vasiliys-MacBook-Pro.local>
2019-09-24 17:01:50 +0300

    Updates

---
+++ precache-manifest.1f109de4cde93829855aecab894feeb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df52fb80aa57b131613bdbfe04d9976e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c9eeefdc8e478c6c83",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "e1befbff572a3db33f10",
+    "url": "/static/css/main.ccf567ba.chunk.css"
+  },
+  {
+    "revision": "02c9eeefdc8e478c6c83",
+    "url": "/static/js/2.d244f02a.chunk.js"
+  },
+  {
+    "revision": "e1befbff572a3db33f10",
+    "url": "/static/js/main.37d25787.chunk.js"
+  },
+  {
+    "revision": "a93d16e7a365dd7024c3",
+    "url": "/static/js/runtime~main.9e4f4ee3.js"
+  }
+]);
